284fcfcee230559ef86e87919a74a84d88e1b823
mihalisfd <xiaomiredminote1982@gmail.com>
2022-08-07 13:22:26 +0300

    Done with part 7 of the tutorial

--- mysite/polls/models.py
+++ mysite/polls/models.py
@@ -1,13 +1,23 @@
 from django.db import models
 from django.utils import timezone
 import datetime
+import pdb
+from django.urls import reverse
+from django.contrib import admin
 
 class Question(models.Model):
     question_text = models.CharField(max_length=200)
     pub_date = models.DateTimeField('date published')
 
+    @admin.display(
+        boolean=True,
+        ordering='pub_date',
+        description='Published recently?',
+    )  
     def was_published_recently(self):
-        return self.pub_date >= timezone.now() - datetime.timedelta(days=1)    
+        now = timezone.now()
+        #pdb.set_trace()
+        return now - datetime.timedelta(days=1) <= self.pub_date <= now
 
     def __str__(self):
         return self.question_text
